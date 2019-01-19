5c50cc29e3c54ed0b071d02be3ef55b42b74ca05
Ivan Vulovic <ivn.vulovic@gmail.com>
2019-01-19 15:07:19 +0100

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Route, Switch, BrowserRouter} from "react-router-dom";
+import { browserHistory } from "react-dom";
 import './App.css';
+
+import Home from "./components/Home";
+import {Menu} from "./components/Menu";
+import {NotFound} from "./components/NotFound";
+import {Other} from "./components/Other";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <BrowserRouter history={browserHistory}>
+          <div>
+            <Menu />
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/other" component={Other}/>
+              <Route component={NotFound} />
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Component} from "react";
+import {Spinner} from "./Spinner";
+
+export default class Home extends Component{
+  render(){
+    return (
+      <div>
+        Home
+      </div>
+    )
+  }
+}
