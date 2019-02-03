adda8ebc15f8351dddba65974a099a3afd7a5352
Ivan Vulovic <ivn.vulovic@gmail.com>
2019-02-03 12:36:00 +0100

    css fixes, note-list updated

--- src/components/project/notes/Note.js
+++ src/components/project/notes/Note.js
@@ -1,29 +1,16 @@
 import React from "react";
 import {RemoveControl} from "../shared/RemoveControl";
 import {UpdateNote} from "./UpdateNote";
-import ExpansionPanel from "@material-ui/core/es/ExpansionPanel/ExpansionPanel";
-import ExpansionPanelSummary from "@material-ui/core/es/ExpansionPanelSummary/ExpansionPanelSummary";
-import {FaChevronDown} from "react-icons/fa";
-import ExpansionPanelDetails from "@material-ui/core/es/ExpansionPanelDetails/ExpansionPanelDetails";
+
 export class Note extends React.Component{
+
   render(){
      return  (
-      <ExpansionPanel className="no-padding no-shadow">
-        <ExpansionPanelSummary className={"no-padding"} expandIcon={<FaChevronDown size={13}/>}>
-          <div>
-            <p className="note-title standard-font-size">{this.props.title}</p>
-          </div>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails className={"no-padding no-margin"}>
-          <div>
-            <p className="text-muted small-bottom-space">Description:</p>
-            <p className="color-gray small-line-spacing text-justify standard-font-size">{this.props.description}</p>
-            <div className="small-bottom-space"> </div>
-            <UpdateNote mode="default"  taskId={this.props.taskId} title={this.props.title} id={this.props.id} description={this.props.description} onUpdate={this.props.onUpdate}/>
-            <RemoveControl mode="default" taskId={this.props.taskId}  topic="note" id={this.props.id} onRemove={this.props.onRemove}/>
-          </div>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
+      <div className="bottom-space">
+         <p className="note-title small-line-spacing standard-font-size text-justify small-bottom-space" onClick={()=>this.props.showNoteDetails(this.props.id)}>
+          {this.props.title}
+        </p>
+       </div>
     );
 
   }
--- src/components/project/notes/NoteList.js
+++ src/components/project/notes/NoteList.js
@@ -1,17 +1,49 @@
 import React from "react";
 import {Note} from "./Note";
+import {UpdateNote} from "./UpdateNote";
+import {RemoveControl} from "../shared/RemoveControl";
 
-export const NoteList = (props) =>(
-  <div>
-    {props.notes.map((note) => <Note key={note.id}
-                                        id={note.id}
-                                        title={note.title}
-                                        description={note.description}
-                                        author={note.author}
-                                        taskId={props.taskId}
-                                        onCreate={props.onCreate}
-                                        onUpdate={props.onUpdate}
-                                        onRemove={props.onRemove}/>)}
-    {!Boolean(props.notes.length) && <p className="text-muted small-margin-top small-bottom-space">We couldn't find any note.</p>}
-  </div>
-);
+export class NoteList extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeNote: {}
+    }
+  }
+
+
+
+  showNoteDetails = (id) => {
+    console.log(id)
+    this.setState({activeNote: this.props.notes.filter(note => note.id === id)[0]})
+  }
+  render(){
+    return (
+      <div>
+        <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 no-padding">
+          {this.props.notes.map((note) => (<p key={note.id} className="note-title small-line-spacing standard-font-size text-justify small-bottom-space" onClick={()=>this.showNoteDetails(note.id)}>{note.title}</p>))}
+        </div>
+        <div className="col-xs-12 col-sm-12 col-md-8 col-lg-8 no-padding">
+          { Object.keys(this.state.activeNote).length !== 0 ? (
+            <div>
+              <p className="text-muted">Title:</p>
+              <p className="standard-font-size color-gray text-justify small-line-spacing small-bottom-space">
+                {this.state.activeNote.title}
+              </p>
+              <p className="text-muted">Description:</p>
+              <p className="standard-font-size color-gray text-justify small-line-spacing">
+                {this.state.activeNote.description}
+              </p>
+              <div>
+                <UpdateNote mode="default"  taskId={this.props.taskId} title={this.state.activeNote.title} id={this.state.activeNote.id} description={this.state.description} onUpdate={this.props.onUpdate}/>
+                <RemoveControl mode="default" taskId={this.props.taskId}  topic="note" id={this.state.activeNote.id} onRemove={this.props.onRemove}/>
+              </div>
+            </div>
+          ) : ''}
+        </div>
+        {!Boolean(this.props.notes.length) && <p className="no-results-paragraph">We couldn't find any note.</p>}
+      </div>
+    );
+  }
+}
